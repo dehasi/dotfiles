3b6040725b2cadeeb6a3efdde11b394a83423db2
Ravil Galeyev <dehasi@proton.me>
2025-08-03 19:31:29 +0200

    none-ls

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,44 +1,65 @@
 return {
-  {
-    "mason-org/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end
-  },
-  {
-    "mason-org/mason-lspconfig.nvim",
-    dependencies = {
-      { "mason-org/mason.nvim", opts = {} },
-      "neovim/nvim-lspconfig",
-    },
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "elp",
-          "golangci_lint_ls",
-          "gopls",
-          "lua_ls",
-          "rust_analyzer",
-          "ts_ls"
-          }
-      })
-    end
-  },
-  {
-    "neovim/nvim-lspconfig",
-    lazy = false,
-    config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.elp.setup({})
-      lspconfig.golangci_lint_ls.setup({})
-      lspconfig.gopls.setup({})
-      lspconfig.lua_ls.setup({})
-      lspconfig.rust_analyzer.setup({})
-      lspconfig.ts_ls.setup({})
+	{
+		"mason-org/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		dependencies = {
+			{ "mason-org/mason.nvim", opts = {} },
+			"neovim/nvim-lspconfig",
+		},
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"elp",
+					"golangci_lint_ls",
+					"gopls",
+					"lua_ls",
+					"rust_analyzer",
+					"ts_ls",
+				},
+			})
+		end,
+	},
+	{
+		"jay-babu/mason-null-ls.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"mason-org/mason.nvim",
+			"nvimtools/none-ls.nvim",
+		},
+		config = function()
+			-- require your null-ls config here (example below)
+			require("mason-null-ls").setup({
+				ensure_installed = {
+					"golangci-lint",
+                    "goimports",
+					"stylua",
+					"jq",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.elp.setup({})
+			lspconfig.golangci_lint_ls.setup({})
+			lspconfig.gopls.setup({
+                completeUnimported = true
+            })
+			lspconfig.lua_ls.setup({})
+			lspconfig.rust_analyzer.setup({})
+			lspconfig.ts_ls.setup({})
 
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set({'n', 'v'}, "<leader>ca", vim.lsp.buf.code_action, {})
-    end
-  }
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
